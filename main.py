b21164a155baf5aae3a53a7d38e28251429a3ad9
eplay1602-afk <eplay1602@gmail.com>
2026-06-12 16:34:17 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,33 +1,51 @@
 import os
 import discord
 from discord.ext import commands
-from discord import app_commands
 
 TOKEN = os.getenv("TOKEN")
-GUILD_ID = 1514939530833301556
 
-intents = discord.Intents.default()
-intents.members = True
+intents = discord.Intents.all()
 
-bot = commands.Bot(command_prefix="!", intents=intents)
+bot = commands.Bot(
+    command_prefix="!",
+    intents=intents
+)
 
 @bot.event
 async def on_ready():
+
     try:
-        guild = discord.Object(id=GUILD_ID)
-        synced = await bot.tree.sync(guild=guild)
-        print(f"Синхронизировано: {len(synced)}")
+        synced = await bot.tree.sync()
+        print(f"Синхронизировано команд: {len(synced)}")
     except Exception as e:
         print(f"Ошибка sync: {e}")
 
     print(f"Бот запущен как {bot.user}")
 
-@bot.tree.command(name="test", description="Проверка")
+@bot.tree.command(
+    name="test",
+    description="Проверка работы"
+)
 async def test(interaction: discord.Interaction):
-    await interaction.response.send_message("Бот работает!")
+
+    await interaction.response.send_message(
+        "✅ Бот работает!"
+    )
+
+@bot.tree.command(
+    name="ping",
+    description="Пинг"
+)
+async def ping(interaction: discord.Interaction):
+
+    await interaction.response.send_message(
+        f"🏓 Pong!"
+    )
 
 @bot.tree.error
 async def on_app_command_error(interaction, error):
-    print(f"ОШИБКА КОМАНДЫ: {repr(error)}")
+
+    print("ОШИБКА КОМАНДЫ:")
+    print(repr(error))
 
 bot.run(TOKEN)
